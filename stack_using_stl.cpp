7f8b753d613131ec0decfada2c2789bc0a2ae182
Ayush-Shambharkar7 <ayushshambharkar0007@gmail.com>
2024-10-14 22:10:00 +0530

    Create stack_using_stl.cpp

---
+++ stack_using_stl.cpp
@@ -0,0 +1,21 @@
+/*
+  Name: Ayush Shambharkar
+  PRN: 23070123034
+  Aim: Stack implementation using stl 
+*/
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int>st;
+st.push(30);
+st.push(20);
+st.push(10);
+
+    // Print the top element of the stack
+cout<< "Top element of the stack: " <<st.top() <<endl;
+
+    return 0;
+}
