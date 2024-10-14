ad4ec78736fe463e0372d2fd1d4308210b1e02c3
Ayush-Shambharkar7 <ayushshambharkar0007@gmail.com>
2024-10-14 22:08:54 +0530

    Create stack_using_array.cpp

---
+++ stack_using_array.cpp
@@ -0,0 +1,63 @@
+/*
+  Name: Ayush Shambharkar
+  PRN: 23070123034
+  Aim: Stack implementation using arrays
+*/
+#include <iostream>
+using namespace std;
+
+class Stack {
+public:
+    int capacity;
+    int top;
+    int* arr;
+
+Stack(int capacity) {
+        this->capacity = capacity;
+arr = new int[capacity];
+        top = -1; 
+    }
+
+    ~Stack() {
+delete[] arr; 
+    }
+
+
+    void push(int element) {
+        if (top < capacity - 1) { 
+            top++;
+arr[top] = element;
+        } else {
+cout<< "Stack Overflow" <<endl;
+        }
+    }
+
+    void pop() {
+        if (top >= 0) {
+            top--;
+        } else {
+cout<< "Stack Underflow" <<endl;
+        }
+    }
+
+    int peek() {
+        if (top >= 0) {
+            return arr[top];
+        } else {
+            return -1; 
+        }
+    }
+};
+
+int main() {
+    Stack st(5);
+st.push(23);
+st.push(24);
+st.push(25);
+
+cout<<st.peek() <<endl; // Output: 25
+st.pop();
+cout<<st.peek() <<endl; // Output: 24
+
+    return 0;
+}
